b25a409f1b27fd9109517e6fb0e236d1b93949af
reman <reman>
2023-09-11 10:05:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a001_intro/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a001_intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText etUN, etPass;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUN=findViewById(R.id.main_etUserName);
+        etPass=findViewById(R.id.main_etPassword);
+        btn=findViewById(R.id.main_btnSetText);
+        etUN.setText("Test UserName");
+        etPass.setText("Test Password");
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "001_intro"
+include(":app")
